6d00cbf513d7bd7f12df9f3492f15c69e6daabc1
howardnm <bueess@163.com>
2025-12-11 23:34:34 +0800

    commit

---
+++ 20_Kafka_producer/05_producer_partition_key.py
@@ -0,0 +1,41 @@
+"""
+演示Kafka的生产者代码
+
+先创建topic主题:
+cd /export/server/kafka
+bin/kafka-topics.sh --create --bootstrap-server ct104:9092,ct105:9092,ct106:9092 --topic testpython --partitions 3
+
+去kafka tool工具查看数据
+
+"""
+# 导入包，导入KafkaProducer对象
+from kafka import KafkaProducer
+import random
+
+# 构建生产者对象(class)
+producer = KafkaProducer(bootstrap_servers=['ct104:9092', 'ct105:9092', 'ct106:9092'])
+
+
+# TODO: partition key 指定key分区发送数据
+
+# 调用生产者的send方法即可发送数据
+for i in range(10):
+    futher = producer.send(
+        topic='testpython',
+        value=f"I love you".encode("UTF-8"), # encode("UTF-8") 等价于 b'I love you'
+        key=b'my', # 指定key发送数据（相同key的数据会被分配到同一个分区，以key的hash值决定分区编号）
+    )
+
+    # 案例： 不同的key发送数据
+    for key in [b'key1', b'key2', b'key3']:
+        futher = producer.send(
+            topic='testpython',
+            value=f"Message for {key.decode()}".encode("UTF-8"),
+            key=key,
+        )
+
+    # 输出futher对象
+    result = futher.get(10) # 阻塞等待10秒钟，获取发送结果
+    print(result.topic) # 输出主题名称
+    print(result.partition) # 输出分区编号
+    print(result.offset) # 输出偏移量，表示当前消息在分区中的位置
